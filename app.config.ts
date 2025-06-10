d91dc5566a6651d0eaed4fc2e0fbff638636b4e6
emmadal <emmanueldalougou@gmail.com>
2025-06-10 16:16:32 +0000

    refactor: change app scheme

--- app.config.ts
+++ app.config.ts
@@ -8,7 +8,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   owner: "emmadal",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
-  scheme: "agenttracker",
+  scheme: "agent",
   userInterfaceStyle: "automatic",
   newArchEnabled: true,
   description:
